82c6314266f979b6fdaa1ed471ac47942744f5df
KINGAL <kingal171314@gmail.com>
2020-09-17 23:33:11 +0800

    add products.py

---
+++ products.py
@@ -0,0 +1,12 @@
+# while loop 通常使用在我們不知道會執行幾次的形況下
+products = []
+while True:
+	name = input('請輸入商品名稱: ')
+	if name == 'q':
+		break
+	price = input('請輸入商品價格: ')
+	products.append([name, price])
+print(products)
+
+
+
